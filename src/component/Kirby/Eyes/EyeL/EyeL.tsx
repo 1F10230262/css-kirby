0215347c3e3a354553289c26ed5f68f1f863080d
1F10230262 <s1F102302629@iniad.org>
2023-10-01 17:08:26 +0900

    EyeLを分けた

---
+++ src/component/Kirby/Eyes/EyeL/EyeL.tsx
@@ -0,0 +1,12 @@
+import styles from './EyeL.module.css';
+
+export const EyeL = () => (
+  <>
+    <div className={styles.eyeLt} />
+    <div className={styles.eyeLr} />
+    <div className={styles.eyeLu} />
+    <div className={styles.eyeLl} />
+    <div className={styles.eyeLm} />
+    <div className={styles.highlightL} />
+  </>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { EyeL } from '../component/Kirby/Eyes/EyeL/EyeL';
 import styles from './index.module.css';
 
 const Home = () => {
@@ -5,13 +6,19 @@ const Home = () => {
     <div className={styles.container}>
       <div className={styles.base}>
         <div className={styles.body} />
-        <div className={styles.eyeL} />
-        <div className={styles.eyeR} />
-        <div className={styles.lightL} />
-        <div className={styles.lightR} />
+        <div className={styles.bodyline} />
+        <div className={styles.bodylineE} />
+        <EyeL />
+        <div className={styles.eyeRt} />
+        <div className={styles.eyeRr} />
+        <div className={styles.eyeRu} />
+        <div className={styles.eyeRl} />
+        <div className={styles.eyeRm} />
+        <div className={styles.highlightR} />
         <div className={styles.cheekL} />
         <div className={styles.cheekR} />
         <div className={styles.mouth} />
+        <div className={styles.mouthE} />
         <div className={styles.handL} />
         <div className={styles.handR} />
         <div className={styles.legL} />
