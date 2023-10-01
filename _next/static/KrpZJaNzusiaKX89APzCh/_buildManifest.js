4b25c5c7e8e407ffce57452573b69eccf605d50e
1F10230262 <1F10230262@users.noreply.github.com>
2023-10-01 08:11:06 +0000

    deploy: 0215347c3e3a354553289c26ed5f68f1f863080d

---
+++ _next/static/KrpZJaNzusiaKX89APzCh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e3010f7c595155fe.css","static/chunks/pages/index-063e63c4774494e9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
